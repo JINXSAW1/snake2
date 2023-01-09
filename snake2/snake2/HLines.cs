5cbaf7511847f38e71ad20899654e88a48d887c7
vanessagav <zelivadmi@gmail.com>
2023-01-09 13:29:17 +0300

    добавление класса горизонтальных линий и самой линии

---
+++ snake2/snake2/HLines.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake2
+{
+    internal class HLines
+    {
+        
+
+        public HLines (int xLeft,int xRight, int y,char sym)
+        {
+            PList = new List<Point> ();
+            for (int x = xLeft; x<= xRight; x++)
+            {
+                Point p = new Point(x,y,sym);
+                PList.Add (p);
+            }
+        }
+
+        public void DrawHLine()
+        {
+            foreach (Point p in PList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
--- snake2/snake2/Program.cs
+++ snake2/snake2/Program.cs
@@ -9,9 +9,13 @@ namespace snake2
     internal class Program
     {
         static void Main(string[] args)
-        {
+            {
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
+
+            HLines Line = new HLines(5, 10, 3,'+');
+            Line.DrawHLine();
+
 
             Console.ReadLine();
         }
